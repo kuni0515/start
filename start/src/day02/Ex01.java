95823356073e37161710da458bed70fd6cdb7fdb
BB2 <BB2@DESKTOP-B6LE7CL>
2022-02-23 18:17:59 +0900

    1st commit

---
+++ start/src/day02/Ex01.java
@@ -0,0 +1,17 @@
+package day02;
+
+import java.util.*;
+public class Ex01 {
+	/*
+	 	참고]
+	 		키보드로 입력된 문자를 읽어오는 방법
+	 			Scanner sc = new Scanner(System.in);
+	 			
+	 			사이에 입력 안내 메세지를 출력해주는 것이 좋다.
+	 			
+	 			String str = sc.nextLine();
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ start/src/day02/Ex02.java
@@ -0,0 +1,10 @@
+package day02;
+
+import javax.swing.*;
+public class Ex02 {
+	public static void main(String[] args) {
+		String str = JOptionPane.showInputDialog("만나고 싶은 가수를 입력");
+		JOptionPane.showMessage
+	}
+
+}
