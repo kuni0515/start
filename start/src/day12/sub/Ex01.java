f50c561da7b82f791869232835d8d131c68d26c8
BB2 <BB2@175.112.42.233>
2022-03-16 22:37:41 +0900

    2022.03.16

---
+++ start/src/day12/sub/Ex01.java
@@ -0,0 +1,22 @@
+package day12.sub;
+
+/*
+	랜덤하게 3 ~ 5 정수를 발생시켜서
+		3 이 나오면 삼각형
+		4 가 나오면 사각형
+		5 가 나오면 원
+	의 형태로 10의 도형을 만들어서 
+	하나의 변수로 관리하고
+	각 도형의 내용을 출력하세요.
+ */
+public class Ex01 {
+	
+	public Ex01() {
+		
+	}
+	
+	public static void main(String[] args) {
+		new Ex01();
+	}
+
+}
